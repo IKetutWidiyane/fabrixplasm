76c81b6751023cfb1fbf5f99f64972feddfe49f4
IKetutWidiyane <iketutwidiane09@gmail.com>
2026-09-04 13:22:03 +0700

    add texture

--- components/hero/SteelPlate.tsx
+++ components/hero/SteelPlate.tsx
@@ -1,15 +1,31 @@
+"use client";
 import { forwardRef } from "react";
 import * as THREE from "three";
+import { useTexture } from "@react-three/drei";
 
 export const SteelPlate = forwardRef<THREE.Group>((_, ref) => {
+  // Load tekstur dari folder public/textures/hero/
+  const textureProps = useTexture({
+    map: "/textures/hero/Metal061A_1K-PNG_Color.png",
+    normalMap: "/textures/hero/Metal061A_1K-PNG_NormalGL.png",
+    roughnessMap: "/textures/hero/Metal061A_1K-PNG_Roughness.png",
+    metalnessMap: "/textures/hero/Metal061A_1K-PNG_Metalness.png",
+  });
+
   return (
     <group ref={ref} position={[0, -1, 0]}>
       <mesh>
-        {/* Diperbesar agar sesuai sudut Top-Down */}
+        {/* Plat besi */}
         <boxGeometry args={[20, 0.4, 15]} />
-        <meshStandardMaterial color="#2A2A2A" roughness={0.7} metalness={0.8} />
+        
+        {/* Sebarkan (spread) properti tekstur ke material */}
+        <meshStandardMaterial 
+          {...textureProps} 
+          envMapIntensity={1} // Memperkuat pantulan cahaya pada area metalik
+        />
       </mesh>
     </group>
   );
 });
+
 SteelPlate.displayName = "SteelPlate";
