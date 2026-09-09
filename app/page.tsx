838ae094f7bf0847c9906dda56b8590eb5c9d27c
IKetutWidiyane <iketutwidiane09@gmail.com>
2026-09-09 15:55:05 +0700

    update'

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
+
 import Hero from "@/components/hero/Hero";
-import  CNCSection  from "@/components/sections/cnc/CNCSection";
+import CNCSection from "@/components/sections/cnc/CNCSection";
+import ProcessSection from "@/components/sections/process/ProcessSection";
 import CustomCursor from "@/components/ui/CustomCursor";
 import { useLenis } from "@/hooks/useLenis";
 
 export default function Home() {
-  useLenis(); // Memanggil smooth scroll globally
+  useLenis();
 
   return (
     <main className="relative min-h-screen">
       <CustomCursor />
+
       <Hero />
       <CNCSection />
+      <ProcessSection />
     </main>
   );
 }
